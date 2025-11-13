4220f24ef62f5aee008b5b18c51881bbca147efe
KeaHeng8888 <lievkeaheng678@gmail.com>
2025-11-13 23:14:18 +0700

    first init

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+//constant
+
+// const decreaseBtn = document.getElementById("decreaseBtn");
+// const resetBtn = document.getElementById("resetBtn");
+// const increaseBtn = document.getElementById("increaseBtn");
+// const countLabel = document.getElementById("countlabel");
+
+// let count = 0;
+
+// decreaseBtn.onclick = function(){
+//     count--;
+//     countLabel.textContent = count;
+// }
+
+// increaseBtn.onclick = function(){
+//     count++;
+//     countLabel.textContent = count;
+// }
+
+
+// resetBtn.onclick = function(){
+//     count = 0;
+//     countLabel.textContent = count;
+// }
---
+++ javascript/index2.js
@@ -0,0 +1,39 @@
+// arithmetic operators = operands (values, variables, etc.) and operators (+, -, *, /, etc.,etc)
+
+//let students =30;
+
+//students = student + 1;
+//students = students - 1;
+//students = students * 5;
+//students = students ** 2;
+//students = students % 3;
+//let extraStudents = students % 3; 
+
+//students += 1;
+//students **= 3
+//students ++;
+//students --;
+
+
+//console.log(students);
+
+//let username;
+
+
+//username = window.prompt("What's your username?");
+
+
+//console.log(username);
+
+
+//let message = (username == "student") ? "Hello Student" : "Hello User";
+
+// let username;
+
+// document.getElementById("mysubmit").onclick = function() {
+//     //username = document.getElementById("mytext").value;
+//     //document.getElementById("myH1").textContent = `Welcome to my first websites ${username}`
+// }
+
+
+
